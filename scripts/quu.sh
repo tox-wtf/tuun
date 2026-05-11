a2505dd37be5d1ff02956b61449410e4adbdaea9
tox <tox@tox.wtf>
2026-05-11 16:37:20 -0500

    feat: clean up scripts

--- scripts/quu.sh
+++ scripts/quu.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 # NOTE: This is just a reference implementation. Feel free to make it your own.
 
@@ -7,7 +7,7 @@ SONG_DIR=$(grep "music_dir = " ~/.config/tuun/config.toml | cut -d'"' -f2)
 SONG_DIR=${SONG_DIR:-${XDG_MUSIC_DIR:-"$HOME/Music"}}
 
 # And ensure it exists
-if [ ! -d "$SONG_DIR" ]; then
+if ! [[ -d $SONG_DIR ]]; then
     echo "Music directory not found: $SONG_DIR" >&2
     exit 1
 fi
@@ -30,15 +30,16 @@ find "$SONG_DIR" -mindepth 1 -type f \
         -e 's,",\\",g'          \
         > /tmp/tuun/_quu.tpl
 
-# Finalize the queue if anything was actually selected; otherwise remove the
-# temporary queue
-if [ -s /tmp/tuun/_quu.tpl ]; then
-    mv /tmp/tuun/_quu.tpl /tmp/tuun/quu.tpl
-else
+# If nothing was selected, remove the queue and exit
+if ! [[ -s /tmp/tuun/_quu.tpl ]]; then
     rm -f /tmp/tuun/_quu.tpl
+    exit 0
 fi
 
+# Finalize the queue if anything was actually selected
+mv /tmp/tuun/_quu.tpl /tmp/tuun/quu.tpl
+
 # Start tuun if something was queued and it isn't running
-if [ -e /tmp/tuun/quu.tpl ] && ! pgrep -x 'tuun' >/dev/null 2>&1; then
+if [[ -e /tmp/tuun/quu.tpl ]] && ! pgrep -x 'tuun' &>/dev/null; then
     alacritty --class tuun --hold -e %BINDIR%/tuun &
 fi
